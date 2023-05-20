bda102f175120dc2c404fb5f618a5036955e701c
cam1lle <117128707+cam1lle@users.noreply.github.com>
2023-05-20 15:03:19 -0600

    Added db

---
+++ crowdfunding_db_schema.sql
@@ -0,0 +1,59 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "campaign" (
+    "cf_id" int   NOT NULL,
+    "contact_id" int   NOT NULL,
+    "company_name" varchar(255)   NOT NULL,
+    "description" varchar(255)   NOT NULL,
+    "goal" float   NOT NULL,
+    "pledged" float   NOT NULL,
+    "outcome" varchar(255)   NOT NULL,
+    "backers_count" int   NOT NULL,
+    "country" varchar(255)   NOT NULL,
+    "currency" varchar(255)   NOT NULL,
+    "launched_date" date   NOT NULL,
+    "end_date" date   NOT NULL,
+    "category_id" varchar(255)   NOT NULL,
+    "subcategory_id" varchar(255)   NOT NULL,
+    CONSTRAINT "pk_campaign" PRIMARY KEY (
+        "cf_id"
+     )
+);
+
+CREATE TABLE "category" (
+    "category_id" varchar(255)   NOT NULL,
+    "category" varchar(255)   NOT NULL,
+    CONSTRAINT "pk_category" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+CREATE TABLE "contact" (
+    "contact_id" int   NOT NULL,
+    "first_name" varchar(255)   NOT NULL,
+    "last_name" varchar(255)   NOT NULL,
+    "email" varchar(255)   NOT NULL,
+    CONSTRAINT "pk_contact" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+CREATE TABLE "subcategory" (
+    "subcategory_id" varchar(255)   NOT NULL,
+    "subcategory" varchar(255)   NOT NULL,
+    CONSTRAINT "pk_subcategory" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "contact" ("contact_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "category" ("category_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "subcategory" ("subcategory_id");
+
